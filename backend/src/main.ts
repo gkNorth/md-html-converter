28245350b365eb38bf5799247ac4c8c92648bf6e
gun.kobayashi <gun.kobayashi@ND-LE192.local>
2023-11-09 12:23:07 +0900

    cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,10 +4,11 @@ import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const env = process.env.NODE_ENV || 'production';
-  if (env === 'develop') {
-    app.enableCors();
-  }
+  // const env = process.env.NODE_ENV || 'production';
+  // if (env === 'develop') {
+  //   app.enableCors();
+  // }
+  app.enableCors();
   app.use(bodyParser.json({ limit: '50mb' }));
   await app.listen(8000);
 }
