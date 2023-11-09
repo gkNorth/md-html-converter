6f1203f9b98591e58fe7c356e059f71ee24bd67a
gun.kobayashi <gun.kobayashi@ND-LE192.local>
2023-11-09 16:18:46 +0900

    puppeteer

--- backend/render-build.sh
+++ backend/render-build.sh
@@ -1,16 +1,16 @@
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
+# #!/usr/bin/env bash
+# # exit on error
+# set -o errexit
 
-STORAGE_DIR=/opt/render/project/.render
+# STORAGE_DIR=/opt/render/project/.render
 
-if [[ ! -d $STORAGE_DIR/chrome ]]; then
-  sudo apt-get install chromium-browser
-else
-  echo "...Using Chrome from cache"
-fi
+# if [[ ! -d $STORAGE_DIR/chrome ]]; then
+#   sudo apt-get install chromium-browser
+# else
+#   echo "...Using Chrome from cache"
+# fi
 
-# be sure to add Chromes location to the PATH as part of your Start Command
-# export PATH="${PATH}:/opt/render/project/.render/chrome/opt/google/chrome"
+# # be sure to add Chromes location to the PATH as part of your Start Command
+# # export PATH="${PATH}:/opt/render/project/.render/chrome/opt/google/chrome"
 
-# add your own build commands...
+# # add your own build commands...
