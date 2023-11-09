10cb38a767567746650c55544d0b8e70e7124d04
gun.kobayashi <gun.kobayashi@ND-LE192.local>
2023-11-09 15:19:04 +0900

    puppeteer

--- backend/render-build.sh
+++ backend/render-build.sh
@@ -10,9 +10,9 @@ if [[ ! -d $STORAGE_DIR/chrome ]]; then
   cd $STORAGE_DIR/chrome
   wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
   dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
+  chmod -R a+rwx $STORAGE_DIR/chrome
   rm ./google-chrome-stable_current_amd64.deb
   cd $HOME/project/src # Make sure we return to where we were
-  chmod -R a+rwx /opt/render/project/.render/chrome
 else
   echo "...Using Chrome from cache"
 fi
