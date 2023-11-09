4aa1698ab2282c3955b4b9aa2139a5ad48debd57
gun.kobayashi <gun.kobayashi@ND-LE192.local>
2023-11-09 09:06:35 +0900

    タイトル修正

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="bg-gray-50">
       <div className="max-w-3xl m-auto">
         <h1 className="text-2xl font-bold text-center text-gray-800 dark:text-white py-4">
-          HTML ↔ Markdown Converter
+          Markdown ↔ HTML Converter
         </h1>
         <div className="container mx-auto p-8">
           <Form />
